6fc05825c854055c8e93bed21c8daaf99ae20e9b
Yuxin Wu <ppwwyyxxc@gmail.com>
2016-12-11 18:54:17 -0500

    the final opengl code

--- test-opengl/main-server.py
+++ test-opengl/main-server.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# File: main.py
+# File: main-server.py
 
 import time
 from nbgl import GLDrawer
 import numpy as np
 import threading
 import sys
+from tensorpack.utils.serialize import loads
 from time import sleep, time
+import zmq
 
 def rand(min = 0.2, max = 1):
     from random import random
@@ -22,11 +24,11 @@ class Sphere(object):
         self.color = [1,1,0]
 
 class Cylinder(object):
-    def __init__(self, p1, p2, r):
+    def __init__(self, p1, p2, r, col):
         self.p1 = np.asarray(p2, dtype='float32')
         self.p2 = np.asarray(p1, dtype='float32')
         self.r = float(r)
-        self.color = [rand() for i in range(3)]
+        self.color = col
 
 class Frame(object):
     def __init__(self, sphlist, cyllist):
@@ -36,21 +38,21 @@ class Frame(object):
 def build_cylinder_from_3dpts(pts):
     ret = []
 
-    def f(id1, id2, thick):
-        return Cylinder(pts[id1,:],pts[id2,:], thick)
+    def f(id1, id2, thick, col=[0,0.5,0]):
+        return Cylinder(pts[id1,:],pts[id2,:], thick, col)
 
-    ret.append(f(0,1, 3))
-    ret.append(f(2,3,1))
-    ret.append(f(3,4,1))
-    ret.append(f(5,6,1))
-    ret.append(f(6,7,1))
+    ret.append(f(0,1,3, [0.5,0.5,0]))
+    ret.append(f(2,3,1, [0, 1, 1]))
+    ret.append(f(3,4,1, [0,1,1]))
+    ret.append(f(5,6,1, [1,0,1]))
+    ret.append(f(6,7,1, [1,0,1]))
     ret.append(f(8,9,1))
     ret.append(f(9,10,1))
     ret.append(f(11,12,1))
     ret.append(f(12,13,1))
     return ret
 
-f = Frame([], [Cylinder([10,10,1],[20,20,2], 3)])
+#f = Frame([], [Cylinder([10,10,1],[20,20,2], 3)])
 
 def run_app(draw_cb, get_frame_func):
     """
@@ -84,16 +86,19 @@ if __name__ == '__main__':
     drawer = GLDrawer('winname', [(-B,B)]*3)
     drawer.start()
 
-    alldata = np.load('p3d.npy')
-    print alldata.shape
     cnt = 0
+
+    ctx = zmq.Context()
+    sok = ctx.socket(zmq.PULL)
+    sok.bind('tcp://0.0.0.0:8888')
     def get_frame():
         global cnt
         cnt += 1
-        step = cnt / 5
-        print step % alldata.shape[0]
-        data = alldata[step%alldata.shape[0]] * 100
+        data = loads(sok.recv(copy=False).bytes)
+        data = data * 100
+        print data
         spheres = [Sphere(3, pos) for pos in data]
+        spheres[0].radius = 10
         cyls = build_cylinder_from_3dpts(data)
         f = Frame(spheres, cyls)
         return f
