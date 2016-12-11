708af97654cb4b83fd70002d095f889a7ca32150
erichuang0771 <ihuangyujia@me.com>
2016-12-10 22:29:33 -0500

    add refine

--- src/refine.py
+++ src/refine.py
@@ -17,37 +17,37 @@ from model import argmean_2d, argmax_2d
 
 
 def searchRegion(target, region):
-	# cv2.imshow("region", region)
- #   	cv2.imshow("target", target)
-	target = target - target.mean()
-	region = region - region.mean()
-	print target.shape
-	print("****")
-	print region.shape
+    # cv2.imshow("region", region)
+ #       cv2.imshow("target", target)
+    target = target - target.mean()
+    region = region - region.mean()
+    print target.shape
+    print("****")
+    print region.shape
 
-	corr = signal.correlate2d(region, target, boundary='symm', mode='same')
-	print corr.shape
+    corr = signal.correlate2d(region, target, boundary='symm', mode='same')
+    print corr.shape
 
-	y, x = np.unravel_index(np.argmax(corr), corr.shape)  # find the match
-	return y,x
-
-
+    y, x = np.unravel_index(np.argmax(corr), corr.shape)  # find the match
+    return y,x
 
 
 
 
 
-PATCH_SIZE = 40
-SEARCH_RANGE = 40
+
+
+PATCH_SIZE = 15
+SEARCH_RANGE = 30
 
 cnt = 0
 trackingPatch = list()
 
 for f in sorted(glob.glob('../recording2/*.npy')):
     buf = open(f).read()
-    m1, m2, o1, o2 = loads(buf)
-    m1 = cv2.cvtColor(m1, cv2.COLOR_BGR2GRAY)
-    m2 = cv2.cvtColor(m2, cv2.COLOR_BGR2GRAY)
+    m1_, m2_, o1, o2 = loads(buf)
+    # m1 = cv2.cvtColor(m1, cv2.COLOR_BGR2GRAY)
+    # m2 = cv2.cvtColor(m2, cv2.COLOR_BGR2GRAY)
 
     pts2d = []
     for k in range(14):
@@ -61,8 +61,8 @@ for f in sorted(glob.glob('../recording2/*.npy')):
         # viz = np.concatenate((m1, m2), axis=1)
         # viz = cv2.resize(viz, (1800,900))
         # interactive_imshow(viz)
-        target_1 = m1[p1[0] - PATCH_SIZE : p1[0] + PATCH_SIZE, 
-        					   p1[1] - PATCH_SIZE : p1[1] + PATCH_SIZE]
+        target_1 = m1_[p1[0] - PATCH_SIZE : p1[0] + PATCH_SIZE, 
+                               p1[1] - PATCH_SIZE : p1[1] + PATCH_SIZE]
 
         # viz = np.concatenate((search_range_1, search_range_2), axis=1)
         # viz = cv2.resize(viz, (SEARCH_RANGE*2,SEARCH_RANGE))
@@ -70,42 +70,62 @@ for f in sorted(glob.glob('../recording2/*.npy')):
 
         # first frame:
         if cnt == 0:
-        	trackingPatch.append(target_1)
+            trackingPatch.append(target_1)
     
     if cnt == 0:
-    	cnt = cnt + 1
-    	continue
+        cnt = cnt + 1
+        continue
 
     for k in range(14):
         p1 = argmax_2d(o1[:,:,k])
         p2 = argmax_2d(o2[:,:,k])
         
 
-    	search_range_1 = m1[p1[0] - SEARCH_RANGE : p1[0] + SEARCH_RANGE, 
-        					   p1[1] - SEARCH_RANGE : p1[1] + SEARCH_RANGE]
+        search_range_1 = m1_[p1[0] - SEARCH_RANGE : p1[0] + SEARCH_RANGE, 
+                               p1[1] - SEARCH_RANGE : p1[1] + SEARCH_RANGE]
 
-    	search_range_2 = m2[p2[0] - SEARCH_RANGE : p2[0] + SEARCH_RANGE, 
-        					   p2[1] - SEARCH_RANGE : p2[1] + SEARCH_RANGE]
-    	
-    	y, x = searchRegion(trackingPatch[k], search_range_1)
-    	
-    	p1_y =  p1[0]  #+ y;
-    	p1_x =  p1[1]  #+ x;
+        search_range_2 = m2_[p2[0] - SEARCH_RANGE : p2[0] + SEARCH_RANGE, 
+                               p2[1] - SEARCH_RANGE : p2[1] + SEARCH_RANGE]
+        
+        # y, x = searchRegion(trackingPatch[k], search_range_1)
+        
+        p1_y =  p1[0]  #+ y;
+        p1_x =  p1[1]  #+ x;
 
-    	tmp = m1[p1[0] - PATCH_SIZE : p1[0] + PATCH_SIZE, 
-        					   p1[1] - PATCH_SIZE : p1[1] + PATCH_SIZE]
+        tmp = m1_[p1[0] - PATCH_SIZE : p1[0] + PATCH_SIZE, 
+                               p1[1] - PATCH_SIZE : p1[1] + PATCH_SIZE]
+
+        m1 = np.copy(m1_)
+        m2 = np.copy(m2_)
+
+        # y, x = searchRegion(tmp, search_range_2)
+        cv2.imshow("target", tmp)
+        
+        result = cv2.matchTemplate(search_range_2,tmp,cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        top_left = max_loc
+
+        w, h,_ = tmp.shape
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(search_range_2,top_left, bottom_right, 255, 2)
+
+        y = top_left[1] + h/2
+        x = top_left[0] + w/2
+
+        cv2.circle(search_range_2, (x,y), 1, (0,0,255), -1)
+        cv2.imshow("search_range_2", search_range_2)
 
 
-    	y, x = searchRegion(tmp, search_range_2)
-    	p2_y =  p2[0]  - SEARCH_RANGE + y;
-    	p2_x =  p2[1]  - SEARCH_RANGE + x;
 
-    	print  p2
-    	print (p2_y, p2_x)
-    	print (y, x)
+        p2_y =  p2[0]   + y - SEARCH_RANGE;
+        p2_x =  p2[1]   + x - SEARCH_RANGE;
+
+        print  p2
+        print (p2_y, p2_x)
+        print (y, x)
     
 
-    	cv2.circle(m1, (int(p1_x),int(p1_y)), 2, (255,255,255), -1)
+        cv2.circle(m1, (int(p1_x),int(p1_y)), 2, (255,255,255), -1)
         cv2.circle(m2, (int(p2_x),int(p2_y)), 2, (255,255,255), -1)
         # cv2.circle(m1, (int(p1[1]),int(p1[0])), 2, (0,0,255), -1)
         # cv2.circle(m2, (int(p2[1]),int(p2[0])), 2, (0,0,255), -1)
